dc2bb8c008a3529cd183fe9c2e9bef7774d9939f
Gaochuang Yang <wolfgao.yang@gmail.com>
2017-07-02 20:47:46 +0800

    Modify some methods of model and view

--- js/moreCats.js
+++ js/moreCats.js
@@ -1,6 +1,6 @@
 $(function(){
     var model = {
-    	//var cats = 0;
+    	currentCat:null,
 		init: function() {
 			var cats=[
 			{name:'Suzy', src:'img/cat01.jpg', count:0},
@@ -12,91 +12,68 @@ $(function(){
 		},
 		getAllCats: function(){
 			return this.cats;
-		},
-		getImage: function (name) {
-			// body...
-			//this.cats.forEach(function(cat){ //can't skip out the array by using array.forEach method, so change to for loop
-			for (i=0; i<this.cats.length;i++) {
-				cat = this.cats[i];
-				if (cat.name == name){
-					//alert(cat.src);
-					return cat.src;
-				}
-			};
-		},
-		addClick: function(name){
-			//this.cats.forEach(function(cat){ //can't skip out the array by using array.forEach method, so change to for loop
-			for (i=0; i<this.cats.length;i++) {
-				cat = this.cats[i];
-				if (cat.name ==name){
-					cat.count +=1;
-					break;
-				}
-			};
-
-		},
-		getClick:function(name){
-			//this.cats.forEach(function(cat){ //can't skip out the array by using array.forEach method, so change to for loop
-			for (i=0; i<this.cats.length;i++) {
-				cat = this.cats[i];
-				if (cat.name ==name){
-					return cat.count;
-				}
-			};
 		}
 	};
 
 	var octopus = {
+		init: function(){
+			var cats = model.getAllCats();
+			model.currentCat = cats[0];
+			catlistView.init(cats);
+			catView.init();
+		},
 		//click the link of each cat name
-		clickCatName: function(name){
-			view.rendor(name);
-			//octopus.clickImage(name);
+		setCurrrentCat: function(cat){
+			model.currentCat = cat;
 		},
 
-		clickImage: function(){
-			$('#img').click(function(){
-				var clickName = $('#catName').html();
-				model.addClick(clickName);
-				var count = model.getClick(clickName);
-				view.rendorCountOnly(count);
+		getCurrentCat: function(){
+			return model.currentCat;
+		},
+		addClick:function(){
+			model.currentCat.count++;
+			catView.rendor();
+		}
+	};
+	var catView = {
+		init: function(name) {
+			this.catNameElem = $('#catName');
+			this.imgElem = $('#img');
+			this.clickCountElem = $('#clickCount');
+			this.imgElem.click(function(){
+				octopus.addClick();
 			});
+		},
+		rendor: function(){
+			var currentCat = octopus.getCurrentCat();
+			this.catNameElem.text(currentCat.name);
+			//$('#img').empty();
+			this.imgElem.attr('src', currentCat.src);
+			//$('#clickCount').empty();
+			this.clickCountElem.text(currentCat.count);
 		}
 	};
 
-	var view = {
-		init: function() {
-			var cats = model.getAllCats();
+	var catlistView = {
+		init: function(cats) {
+			this.catListElem = $('#catlist');
 			for (var i = 0; i<cats.length; i++) {
-				var name = cats[i].name;
-				var catNameElem = $('<ul style="width=30%;"><li><span id="'+name+'">'+name+'</span></li></ul>');
-				$("#catlist").append(catNameElem);
-				var catName = document.getElementById(name);
-				catName.addEventListener('click', (function(nameCopy) {
+				var cat = cats[i];
+				var catNameElem = $('<ul style="width=30%;"><li><span id="'+cat.name+'">'+cat.name+'</span></li></ul>');
+				this.catListElem.append(catNameElem);
+				this.catName = document.getElementById(cat.name);
+				this.catName.addEventListener('click', (function(catCopy) {
 					return function() {
-				    	octopus.clickCatName(nameCopy);
+				    	octopus.setCurrrentCat(catCopy);
+				    	catView.rendor();
 					};
-				})(name));
+				})(cat));
 				
 			};
-		},
-		rendor: function(name) {
-			var imgSrc = model.getImage(name);
-			//alert(name+"  "+imgSrc);
-			var count = model.getClick(name);
-			//$('#catName').empty();
-			$('#catName').text(name);
-			//$('#img').empty();
-			$('#img').attr('src', imgSrc);
-			//$('#clickCount').empty();
-			$('#clickCount').text(count);
-
-			octopus.clickImage();
-		},
-		rendorCountOnly: function(count){
-			$('#clickCount').text(count);
 		}
 	};
+
 	//Initialization model and view.
 	model.init();
-	view.init();
+	octopus.init();
 });
